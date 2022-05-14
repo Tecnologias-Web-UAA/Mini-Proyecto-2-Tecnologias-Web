92af527238be1c43e04755def69ce4583cbf2138
LuisNavarro24 <83561102+LuisNavarro24@users.noreply.github.com>
2022-05-14 10:13:57 -0500

    Informacion heroes
    Informacion heroes actualizada

--- src/app/misheroes.ts
+++ src/app/misheroes.ts
@@ -52,51 +52,51 @@ export const HEROES:Heroe[]=[
       },
       {
         nombre: "Nightwing",
-        bio: "",
+        bio: "Ricardo Tapia fue el mas joven de una familia de acrobatas, el cual no tiene poderes. Destaca por sus habilidades como detective, peleador nato y un experto en el uso de dispositivos y armamento de alta tecnología. Dick Grayson ha asumido la identidad de Batman en algunas ocasiones.",
         img: "assets/img/nightwing.png",
-        aparicion: "",
+        aparicion: "1940-05-05",
         casa: "DC"
       },
       {
         nombre: "Cat woman",
-        bio: "",
+        bio: "Catwoman es una ladrona de Gotham City que normalmente usa un traje ajustado de una sola pieza y usa un látigo como arma. Originalmente fue caracterizada como una supervillana y adversaria de Batman, pero desde la década de 1990 ha aparecido en una serie que la presenta como una antiheroína, a menudo haciendo cosas incorrectas por las razones correctas. En general, no cuenta con superpoderes, pero es una peleadora nata así como una perfecta ladrona.",
         img: "assets/img/catwoman.png",
-        aparicion: "",
+        aparicion: "-1940-01-03",
         casa: "DC"
       },
       {
         nombre: "rocket",
-        bio: "",
+        bio: "89P13, también conocido como Rocket, es un individuo genéticamente modificado parecido a un mapache que se convirtió en un criminal al igual que su amigo Groot. Juntos, viajaron ganando recompensas hasta que conocieron a los Guardianes de la Galaxia, con quienes se alió para detener a Ronan de destruir Xandar. Rocket en realidad no tiene poderes, pero fue mejorado genetica y tecnologicamente para ser mas fuerte.",
         img: "assets/img/rocket.png",
-        aparicion: "",
+        aparicion: "1976-02-04",
         casa: "Marvel"
       },
       {
         nombre: "Doctor Strange",
-        bio: "",
+        bio: "Stephen Strange es un médico especializado en neurocirugía, codicioso y egocéntrico, que solo se preocupa por la riqueza de su carrera, hasta que en un accidente sufrió una enfermedad nerviosa en sus manos que le obligó a retirarse. Al intentar curarse, descubre poderes misticos y puede modificar la realidad, así como crear portales para poder moverse entre ellos de manera mas sencilla por todo el universo.",
         img: "assets/img/doctor-strange.png",
-        aparicion: "",
+        aparicion: "1982-03-05",
         casa: "Marvel"
       },
       {
         nombre: "Capitan America",
-        bio: "",
+        bio: "Steve es un hombre agradable y generoso y ayuda a su equipo siempre que sea posible. Se mantiene fiel a los ideales valores estadounidenses y sacrificaría su vida por cualquier otro buen ciudadano. Steve es un líder valiente e independiente para su equipo. Sus modificaciones geneticas en la segunda guerra mundial le permitieron sobrevivir en el polo norte. Sus poderes mas notables son el combate cuerpo a cuerpo y su inmortalidad. ",
         img: "assets/img/capitan-america.png",
-        aparicion: "",
+        aparicion: "1918-07-04",
         casa: "Marvel"
       },
       {
         nombre: "Flash",
-        bio: "",
+        bio: "Su poder consiste principalmente en la super velocidad. Varios otros efectos también se atribuyen a su capacidad para controlar la velocidad de las vibraciones moleculares, incluida su capacidad para vibrar a la velocidad para pasar a través de los objetos y viajar por el tiempo. Sus especialidades son su inmensa velocidad, agilidad, resistencia sobrehumanas y curación muy rápida",
         img: "assets/img/flash.png",
-        aparicion: "",
+        aparicion: "04-05-1940",
         casa: "DC"
       },
       {
         nombre: "Super Man",
-        bio: "",
+        bio: "El super hombre tiene muchos superpoderes que recalcar. Lo que mas destaca en el es la Súper fuerza, velocidad, resistencia, agilidad, reflejos, durabilidad, sentidos y longevidad. Tambien cuenta con aliento helado y una variedad muy grande de poderes oculares con el cual puede ver a traves de las cosas e incluso lanzar rayos x mediante los ojos.",
         img: "assets/img/superman.png",
-        aparicion: "",
+        aparicion: "1938-18-04",
         casa: "DC"
       },
 ];
